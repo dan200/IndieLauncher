feef162f8fe6446bfc8e388207971e8cb2abd2b3
Daniel Ratcliffe <dratcliffe@gmail.com>
2014-08-11 10:27:11 +0100

    Updated LanguageExport. Specified en translations only

--- UpdateLoc.sh
+++ UpdateLoc.sh
@@ -4,4 +4,5 @@ mono \
 	LanguageExport/Precompiled/LanguageExport.exe \
 	https://docs.google.com/spreadsheets/d/18FjL2zScJZNa1Eqgl3jAjQh_4YB6PZI0JzvbKV4DHUc/edit?usp=sharing \
 	IndieLauncher/Resources/Languages \
+        en \
 	> /dev/null
